76e6ccbd5d59d7bc8d0bf8ce65462c02827e7234
Kobiljon <qobiljonnajmiddinov@gmail.com>
2023-06-16 17:45:20 +0500

    functionality fix, output rule, export on js file, production built

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,14 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     mode: 'development',
     devtool: 'source-map',
     module: {
@@ -16,12 +20,13 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
+
     plugins: [
-        new HtmlWebPackPlugin({
+        new HtmlWebpackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
